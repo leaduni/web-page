306114c42c1b6d9af06005f43b893353975aa61b
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-06-01 15:50:54 -0500

    fix: Colocando router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from 'react-router';
-
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/header';
 // Importamos los componentes de las páginas que vamos a usar
 // En este caso, Home y About son componentes que representan páginas
 // de nuestra aplicación
@@ -25,16 +25,20 @@ import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/application" element={<ApplicationPage />} />
-      <Route path="/news" element={<NewsPage />} />
-      <Route path="/pillars" element={<PillarsPage />} />
-      <Route path="/organization" element={<OrganizationPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-      {/* La ruta "*" captura todas las rutas que no coinciden con las anteriores */}
-      {/* Esto es útil para mostrar una página 404 o Not Found */}
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/application" element={<ApplicationPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/pillars" element={<PillarsPage />} />
+        <Route path="/news/:id" element={<NewsDetailPage />} />
+        <Route path="/organization" element={<OrganizationPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        {/* La ruta "*" captura todas las rutas que no coinciden con las anteriores */}
+        {/* Esto es útil para mostrar una página 404 o Not Found */}
+      </Routes>
+    </>
   );
 }
 
