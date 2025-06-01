5d1b2a0a501e1ae5d62c88695d23c4d3f23407af
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-06-01 15:47:39 -0500

    fix: resolviendo problemas de integracion

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
 import './index.css';
 import App from './App.jsx';
 
