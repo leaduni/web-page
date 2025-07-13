7785dcdeaa52201e5744d2fa524f89f064dd8507
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-07-13 14:02:32 -0500

    fix: cambio en el nombre de news

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -91,8 +91,8 @@ export default function NewsPage() {
       <div className="container mx-auto px-4 ">
         <div className="text-center mb-6">
           <h1 className="text-5xl md:text-6xl font-bold text-center mb-4">
-            <span className="bg-gradient-to-r from-[#FF1CF7] to-[#00F0FF] text-transparent bg-clip-text">
-              LEAD News
+            <span className="bg-gradient-to-r py-3 from-[#FF1CF7] to-[#00F0FF] text-transparent bg-clip-text">
+              Últimas Noticias
             </span>
           </h1>
           <p className="text-gray-400 max-w-2xl mx-auto mb-10">
